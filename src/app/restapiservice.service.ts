385273b7ac7b9975c0181e1863768073d3bf27f0
diurnoguillaume <119867867+diurnoguillaume@users.noreply.github.com>
2022-12-16 15:01:18 +0100

    1 file modified

--- src/app/restapiservice.service.ts
+++ src/app/restapiservice.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpRequest,
+} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RestApiServices {
   url = 'http://localhost/webservice/rest/server.php';
-  params = new HttpParams({
-    fromObject: {
-      wstoken: '0a027d4c25c6c8cfe631f2714a745a12',
-      wsfunction: 'gradereport_user_get_grade_items',
-      moodlewsrestformat: 'json',
-      courseid: '4',
-    },
+  params = JSON.stringify({
+    wstoken: '0a027d4c25c6c8cfe631f2714a745a12',
+    wsfunction: 'gradereport_user_get_grade_items',
+    moodlewsrestformat: 'json',
+    courseid: '4',
   });
-  header = new HttpHeaders({
-    'Content-Type': 'application/x-www-form-urlencoded',
-  });
-
+  header = {
+    'Content-Type': 'application/json',
+    Host: 'localhost',
+    'Content-Length': '406',
+  };
   constructor(private http: HttpClient) {}
 
   getAllCourses() {
-    return this.http.post(this.url, this.params, this.header);
+    return this.http.post(this.url, this.params, { headers: this.header });
   }
 }
